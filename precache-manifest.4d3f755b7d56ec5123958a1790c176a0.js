7322a185920fc12575dde01fe282291bebcb9136
fcastrogarcia <fede.castrog@gmail.com>
2019-03-27 22:11:21 -0300

    Updates

---
+++ precache-manifest.4d3f755b7d56ec5123958a1790c176a0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "aa817215ef5e490bad59",
+    "url": "/pomodoro-clock/static/js/runtime~main.647dc629.js"
+  },
+  {
+    "revision": "6e61b12ab8f92970ed4d",
+    "url": "/pomodoro-clock/static/js/main.758d5b42.chunk.js"
+  },
+  {
+    "revision": "1bd86f64a1299ecbb28f",
+    "url": "/pomodoro-clock/static/js/2.3bcbfa49.chunk.js"
+  },
+  {
+    "revision": "6e61b12ab8f92970ed4d",
+    "url": "/pomodoro-clock/static/css/main.39cefbd3.chunk.css"
+  },
+  {
+    "revision": "23c774aa39c423603f03d72cbfdba95d",
+    "url": "/pomodoro-clock/index.html"
+  }
+];
